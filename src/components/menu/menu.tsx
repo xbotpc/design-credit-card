b975b8d31a3664b436bc72e63a3054df9e0619c8
Piyush <piyush.choughule@acc.ltd>
2021-05-09 14:32:22 +0530

    moved controls to new component

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,10 +1,60 @@
+import { useDispatch, useSelector } from "react-redux"
+import { ACTIONS } from "../../state/actions";
+import InitialState from "../../types/initialState";
+import styles from './menu.module.scss';
+
 const Menu = () => {
+    const { bgColor, imageSize, opacityValue } = useSelector((state: InitialState) => state);
+    const dispatch = useDispatch();
+
     return (
         <>
-            {/* <input type="color" onChange={onBGColorChange} value={cardFrontBGColor} />
-            <input type="range" name="" id="" value={bgImageSize} onChange={onSizeChange} min="1" max="100" />
-            <input type="range" name="" id="" value={opacityValue} onChange={onOpacityChange} min="10" max="100" /> */}
-            {/* <input type="color" onChange={onPatternColorChange} value={patternColor} /> */}
+            <div className={styles.menuContainer}>
+                <div className={styles.menuItem}>
+                    <label htmlFor="color">COLOR:</label>
+                    <input
+                        id="color"
+                        type="color"
+                        value={bgColor}
+                        onChange={(e) => {
+                            dispatch({
+                                type: ACTIONS.BG_COLOR,
+                                payload: e.target.value,
+                            })
+                        }}
+                    />
+                </div>
+                <div className={styles.menuItem}>
+                    <label htmlFor="patternSize">SIZE:</label>
+                    <input
+                        id={"patternSize"}
+                        value={imageSize}
+                        onChange={(e) => {
+                            dispatch({
+                                type: ACTIONS.IMAGE_SIZE,
+                                payload: e.target.value,
+                            })
+                        }}
+                        min="40"
+                        max="100"
+                        type="range"
+                        name=""
+                    />
+                </div>
+                {/* <input
+                type="range"
+                name=""
+                id=""
+                value={opacityValue}
+                onChange={(e) => {
+                    dispatch({
+                        type: ACTIONS.OPACITY,
+                        payload: e.target.value,
+                    })
+                }}
+                min="10" max="100" /> */}
+                {/* <input type="color" onChange={onPatternColorChange} value={patternColor} /> */}
+            </div>
         </>
     )
 }
